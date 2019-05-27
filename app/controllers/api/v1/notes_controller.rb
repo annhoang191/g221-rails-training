f02ac0b0a4a6af0e0256e9b2a76717c47489020d
DuongDo317 <duongdo317@gmail.com>
2019-05-27 18:56:42 +0700

    Task_241611 Update notes

--- app/controllers/api/v1/notes_controller.rb
+++ app/controllers/api/v1/notes_controller.rb
@@ -2,16 +2,26 @@ module Api
   module V1
     class NotesController < ApplicationController
       before_action :authorize_request
+      before_action :find_note, only: %i[update destroy]
 
       def create
         note = current_user.notes.create! note_params
         represent_response NotePresenter.new(note)
       end
 
+      def update
+        @note.update_attributes! note_params
+        represent_response NotePresenter.new(@note)
+      end
+
       private
 
       def note_params
         params.permit(:title, :content)
+      end
+
+      def find_note
+        @note = Note.find params[:id]
       end
     end
   end
--- spec/controller/notes_controller_spec.rb
+++ spec/controller/notes_controller_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe Api::V1::Note, type: :request do
     allow_any_instance_of(Api::V1::NotesController).to receive(:authorize_request) { true }
     allow_any_instance_of(Api::V1::NotesController).to receive(:current_user) { user }
   end
+
   describe "POST /api/v1/note" do
     context "when create note " do
       before(:each) do
@@ -52,6 +53,58 @@ RSpec.describe Api::V1::Note, type: :request do
             title: nil,
             content: "aaaaaaaaaa",
             updated_at: Note.first.updated_at
+          }
+        }.to_json)
+      end
+    end
+  end
+
+  describe "Patch /api/v1/note" do
+    let!(:note) { create(:note, user_id: user.id) }
+    context "when update note" do
+      before do
+        patch "/api/v1/notes/#{note.id}", params: { title: "test", content: "aaaaaaaaaa" }
+      end
+      it "result success" do
+        expect(response.body).to eq({
+          status: true,
+          data: {
+            id: Note.first.id,
+            title: "test",
+            content: "aaaaaaaaaa",
+            updated_at: Note.first.updated_at
+          }
+        }.to_json)
+      end
+    end
+
+    context "when content is null" do
+      before do
+        patch "/api/v1/notes/#{note.id}", params: { title: "test", content: nil }
+      end
+      it "result success" do
+        expect(response.body).to eq({
+          status: false,
+          error: {
+            error_code: 400,
+            message: "Record Invalid",
+            errors: "Content can't be blank"
+          }
+        }.to_json)
+      end
+    end
+
+    context "when note not exist" do
+      before do
+        patch "/api/v1/notes/nil", params: { title: "test", content: "aaaaaaaaaa" }
+      end
+      it "result success" do
+        expect(response.body).to eq({
+          status: false,
+          error: {
+            error_code: 404,
+            message: "Record Not Found",
+            errors: "Couldn't find Note with 'id'=nil"
           }
         }.to_json)
       end
